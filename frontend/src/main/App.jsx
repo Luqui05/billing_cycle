0f92b48573f0948ff671ccc4daa8286a4f839d20
Luqui-05 <lucashtribeiro@hotmail.com>
2025-05-08 16:24:21 -0300

    feat: [frontend] componentes sidebar e menu

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,11 +1,13 @@
 import '../common/template/dependencies'
 import React from "react";
 import Header from '../common/template/header';
+import Sidebar from '../common/template/sidebar';
 
 export default (props) => {
     return (
         <div className="wrapper">
             <Header />
+            <Sidebar />
         </div>
     );
 };
