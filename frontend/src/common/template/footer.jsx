a3f7b5023e1ca29d73fd7355c44793edb05c298a
Luqui-05 <lucashtribeiro@hotmail.com>
2025-05-09 09:20:19 -0300

    feat: [frontend] footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2025
+            <a href="https://luqui05.github.io/pw-portfolio/" target="_blank"> Lucas</a>.
+        </strong>
+    </footer>
+)
