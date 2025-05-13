f95fb54f9d87aa2811aecf4def56cb1d8cb14363
Luqui-05 <lucashtribeiro@hotmail.com>
2025-05-13 09:28:05 -0300

    feat: [frontend] visibilidade das abas

--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -3,22 +3,27 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 
 import { selectTab } from "./tabActions";
+import If from "../operador/if";
 
 class TabHeader extends Component {
     render() {
-        const selected = this.props.tab.selected === this.props.target
+        const selected = this.props.tab.selected === this.props.target;
+        const visible = this.props.tab.visible[this.props.target];
+
         return (
-            <li className={ selected ? 'active' : '' }>
-                <a
-                    href="javascrip:;"
-                    data-toggle="tab"
-                    onClick = {() => this.props.selectTab(this.props.target)}
-                    data-target={this.props.target}
-                >
-                    <i className={`fa fa-${this.props.icon}`}></i>{" "}
-                    {this.props.label}
-                </a>
-            </li>
+            <If test={visible}>
+                <li className={selected ? "active" : ""}>
+                    <a
+                        href="javascrip:;"
+                        data-toggle="tab"
+                        onClick={() => this.props.selectTab(this.props.target)}
+                        data-target={this.props.target}
+                    >
+                        <i className={`fa fa-${this.props.icon}`}></i>{" "}
+                        {this.props.label}
+                    </a>
+                </li>
+            </If>
         );
     }
 }
@@ -26,4 +31,4 @@ class TabHeader extends Component {
 const mapStateToProps = (state) => ({ tab: state.tab });
 const mapDispatchToProps = (dispatch) =>
     bindActionCreators({ selectTab }, dispatch);
-export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader);
