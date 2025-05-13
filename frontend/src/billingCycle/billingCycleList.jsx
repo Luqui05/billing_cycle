4da334c965345fdbf9541b0e7f6280754f512e86
Luqui-05 <lucashtribeiro@hotmail.com>
2025-05-13 11:28:07 -0300

    feat: [frontend] incluindo dados na billingCycleList

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -10,6 +10,13 @@ class BillingCycleList extends Component {
 
     renderRows() {
         const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
     }
     
     render() {
