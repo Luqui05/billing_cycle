1505df1dcf8c74d587a65e60fb6bed9da868849a
Luqui-05 <lucashtribeiro@hotmail.com>
2025-05-16 16:15:53 -0300

    feat [frontend] melhoria na inclusão do ciclo de pagamento

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,8 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import promise from "redux-promise";
+import multi from "redux-multi";
+import thunk from "redux-thunk";
 
 import App from "./main/App";
 import reducers from "./main/reducers";
@@ -12,7 +14,10 @@ import { applyMiddleware } from "redux";
 const devTools =
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = applyMiddleware(promise)(createStore)(reducers, devTools);
+const store = applyMiddleware(multi, thunk, promise)(createStore)(
+    reducers,
+    devTools
+);
 ReactDOM.render(
     <Provider store={store}>
         <App />
