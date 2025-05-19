7789169446f48c4c3bfee37e561283355254c958
Luqui-05 <lucashtribeiro@hotmail.com>
2025-05-19 09:12:11 -0300

    feat: [frontend] botão cancelar

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -4,6 +4,7 @@ import { initialize, reset as resetForm } from "redux-form";
 import { showTabs, selectTab } from "../common/tab/tabActions";
 
 const BASE_URL = "http://localhost:3000/api";
+const INITIAL_VALUES = {};
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`);
@@ -17,20 +18,24 @@ export function create(values) {
     return (dispatch) => {
         axios.post(`${BASE_URL}/billingCycles`, values).then((resp) => {
             toastr.success("Sucesso", "Operação realizada com sucesso");
-            dispatch([
-                resetForm("billingCycleForm"),
-                getList(),
-                selectTab("tabList"),
-                showTabs('tabList', 'tabCreate')
-            ]);
+            dispatch(init());
         });
     };
 }
 
 export function showUpdate(billingCycle) {
-  return [
-    showTabs('tabUpdate'),
-    selectTab('tabUpdate'),
-    initialize('billingCycleForm', billingCycle)
-  ]
+    return [
+        showTabs("tabUpdate"),
+        selectTab("tabUpdate"),
+        initialize("billingCycleForm", billingCycle),
+    ];
+}
+
+export function init() {
+    return [
+        showTabs("tabList", "tabCreate"),
+        selectTab("taList"),
+        getList(),
+        initialize("billingCycleForm", INITIAL_VALUES),
+    ];
 }
